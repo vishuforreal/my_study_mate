149c8353010f8e9334a73fc0be61391222d5d2a7
Vishwajeet <vishuuforreal@gmail.com>
2025-12-11 21:32:07 +0530

    Add MONGODB_URI validation

--- backend/server.js
+++ backend/server.js
@@ -91,6 +91,10 @@ app.use((err, req, res, next) => {
 
 const connectDB = async () => {
     try {
+        if (!process.env.MONGODB_URI) {
+            throw new Error('MONGODB_URI environment variable is not set');
+        }
+        
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
